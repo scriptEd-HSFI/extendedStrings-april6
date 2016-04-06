975f08672d6c6432b935cde1117def2dcf07055f
Clinton Halpin <clintonhalpin@gmail.com>
2016-04-06 07:27:42 -0400

    Initial commit

---
+++ starter.js
@@ -0,0 +1,70 @@
+/* 
+Instructions: Paste this in JSBIN or you can fork and clone in Cloud 9 
+
+1. Ask the user for their full (first and last) name. Store it in a variable and then print it.
+2. Put the first name in its own variable. Repeat for the last name. Print each variable.
+3. Print the last letter of their first name and the first letter of their last name.
+4. Add the following to the beginning of the first name variable: "Your name is ". Print it.
+5. Add the following to the end of the last name variable: "!". Print it.
+6. Concatenate the first name and the last name together and store it in a new variable called x. Print it.
+7. Ask the user how old they are. Store it in a variable and then print it.
+8. Add the following to the end of variable x: "And you're [age] years old!". Print x.
+
+*/
+
+
+console.log("**********START OF EXERCISE 1**********");
+
+// Your code goes here
+
+console.log("**********END OF EXERCISE 1**********");
+
+
+console.log("**********START OF EXERCISE 2**********");
+
+// Your code goes here
+
+console.log("**********END OF EXERCISE 2**********");
+
+
+console.log("**********START OF EXERCISE 3**********");
+
+// Your code goes here
+
+console.log("**********END OF EXERCISE 3**********");
+
+
+console.log("**********START OF EXERCISE 4**********");
+
+// Your code goes here
+
+console.log("**********END OF EXERCISE 4**********");
+
+
+console.log("**********START OF EXERCISE 5**********");
+
+// Your code goes here
+
+console.log("**********END OF EXERCISE 5**********");
+
+
+console.log("**********START OF EXERCISE 6**********");
+
+// Your code goes here
+
+console.log("**********END OF EXERCISE 6**********");
+
+
+console.log("**********START OF EXERCISE 7**********");
+
+// Your code goes here
+
+console.log("**********END OF EXERCISE 7**********");
+
+
+console.log("**********START OF EXERCISE 8**********");
+
+// Your code goes here
+
+console.log("**********END OF EXERCISE 8**********");
+
